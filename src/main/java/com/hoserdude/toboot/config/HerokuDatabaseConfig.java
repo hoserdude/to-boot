85f4a068dcb54027658e28314fd2b5f477cf229d
hoserdude <tim@hoserdude.com>
2014-10-11 02:03:02 -0700

    trying again

--- src/main/java/com/hoserdude/toboot/config/HerokuDatabaseConfig.java
+++ src/main/java/com/hoserdude/toboot/config/HerokuDatabaseConfig.java
@@ -1,16 +1,17 @@
 package com.hoserdude.toboot.config;
 
 import com.mongodb.MongoURI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
+import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -20,6 +21,8 @@ import java.sql.SQLException;
 @Profile("heroku")
 @Configuration
 public class HerokuDatabaseConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(HerokuDatabaseConfig.class);
     @Bean
     public MongoDbFactory mongoDbFactory() throws Exception {
         MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
@@ -27,13 +30,20 @@ public class HerokuDatabaseConfig {
     }
 
     @Bean
-    public Connection getConnection() throws URISyntaxException, SQLException {
+    public DataSource dataSource() throws URISyntaxException, SQLException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
 
-        return DriverManager.getConnection(dbUrl, username, password);
+        logger.info("Setting up DataSource for DB: {}", dbUrl);
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
