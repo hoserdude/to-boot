67b277af9e3332ef8a2966ce206442485ac4c2f7
hoserdude <tim@hoserdude.com>
2014-11-25 22:03:28 -0800

    Removing @Repository annotation from Mongo repo, causing issues with latest Boot release

--- src/main/java/com/hoserdude/toboot/documentRepository/TodoRepository.java
+++ src/main/java/com/hoserdude/toboot/documentRepository/TodoRepository.java
@@ -2,14 +2,12 @@ package com.hoserdude.toboot.documentRepository;
 
 import com.hoserdude.toboot.document.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Magic interface to communicate with MongoDB.
  */
-@Repository
 public interface TodoRepository extends MongoRepository<Todo, String> {
     Todo findByUserIdAndId(String userId, String id);
     List<Todo> findByUserId(String userId);
